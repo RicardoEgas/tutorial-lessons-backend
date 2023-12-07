1983f039b85e02b6c54aa1a0f4d4e9ee88f6b38a
AliReza <ali.reza.buet@gmail.com>
2023-12-08 02:17:43 +0600

    Add sign in api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     respond_to :json
-    
+  
     def create
-        super do |resource|
-            render json: { user: resource, token: resource.authentication_token }
-            return
-        end
+      user = User.find_for_database_authentication(email: params[:user][:email])
+  
+      if user && user.valid_password?(params[:user][:password])
+        sign_in(user, store: false)  # Sign in the user without storing in the session
+        render json: { user: user, message: 'Signed in successfully' }
+      else
+        render json: { error: 'Invalid email or password' }, status: :unauthorized
+      end
     end
-
+  
     def destroy
-        super
+      sign_out(current_user)
+      render json: { message: 'Signed out successfully' }
     end
-end
+  end
