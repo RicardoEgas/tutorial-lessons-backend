e56787437af3287de1f75969789dcf844329352d
AliReza <ali.reza.buet@gmail.com>
2023-12-08 01:27:48 +0600

    Configure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, if: :api_request?
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def api_request?
+        request.format.json?
+    end
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u|
+            u.permit(:name, :email, :password, :password_confirmation)
+        end
+        devise_parameter_sanitizer.permit(:account_update) do |u|
+         u.permit(:name, :email, :password, :current_password)
+        end
+    end
 end
