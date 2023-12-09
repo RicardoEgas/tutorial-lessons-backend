ab1ba427175f71c632e2cd50f5f097372965ae14
AliReza <ali.reza.buet@gmail.com>
2023-12-10 02:17:10 +0600

    Update reservation controller. current_user not working

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
-    before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+    # before_action :authenticate_user!
   before_action :set_tutorial
   before_action :set_reservation, only: [:destroy]
 
@@ -9,6 +10,7 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   def create
+    puts @current_user
     reservation = @tutorial.reservations.build(user: @current_user, reserve_date: params[:reserve_date])
 
     if reservation.save
