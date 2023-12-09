82230547d589babcd242679fa7435c09fa429897
AliReza <ali.reza.buet@gmail.com>
2023-12-10 01:19:58 +0600

    Update controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
     before_action :authenticate_user!, only: [:show]
 
   def show
-    render json: current_user, status: :ok
+    render json: @current_user, status: :ok
   end
 end
