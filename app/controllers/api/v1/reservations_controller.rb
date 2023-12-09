f967f6ee2bf38307e312db5368f45aca9252a52f
AliReza <ali.reza.buet@gmail.com>
2023-12-10 01:00:16 +0600

    Add reservations controller

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::ReservationsController < ApplicationController
+    before_action :authenticate_user!
+  before_action :set_tutorial
+  before_action :set_reservation, only: [:destroy]
+
+  def index
+    reservations = @tutorial.reservations
+    render json: reservations, status: :ok
+  end
+
+  def create
+    reservation = @tutorial.reservations.build(user: @current_user, reserve_date: params[:reserve_date])
+
+    if reservation.save
+      render json: reservation, status: :created
+    else
+      render json: { errors: reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_tutorial
+    @tutorial = Tutorial.find(params[:tutorial_id])
+  end
+
+  def set_reservation
+    @reservation = @tutorial.reservations.find(params[:id])
+  end
+end
