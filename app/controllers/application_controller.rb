53f9c6ebeefbb8c891945094da33ee2aba387bcc
AliReza <ali.reza.buet@gmail.com>
2023-12-13 03:07:37 +0600

    Fix the current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, if: :api_request?, unless: :devise_controller?
   before_action :debug_current_user, if: :current_user_present?, unless: :devise_controller?
-  before_action :set_current_user
+  before_action :current_user
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -30,9 +30,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_current_user
+  def current_user
     return unless session[:user_id]
-
-    @set_current_user ||= User.find_by(id: session[:user_id])
+    
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
