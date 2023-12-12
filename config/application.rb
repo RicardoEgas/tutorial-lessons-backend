0059d8857c408b45f8099283c9acbc4737db2523
AliReza <ali.reza.buet@gmail.com>
2023-12-13 02:13:02 +0600

    Resolve the cors policy

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module TutorialLessonsBackend
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001', 'http://127.0.0.1:3001' # Replace with the origin of your React app
+        resource '/api/*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
