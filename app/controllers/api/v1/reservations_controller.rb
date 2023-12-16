1ff8f1763ea2ec6767e03b11f9c3627b681691ee
AliReza <ali.reza.buet@gmail.com>
2023-12-16 12:12:58 +0600

    Update api doc

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authenticate_user_by_token
-  before_action :set_tutorial, only: %i[index create destroy]
+  before_action :set_tutorial, only: %i[index create destroy show]
   before_action :set_reservation, only: [:destroy, :show]
 
   def index
@@ -9,6 +9,11 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   def show
+    if @reservation.user.id == @current_user.id
+      render json: { reservation: @reservation, message: 'reservation' }, status: :ok
+    else
+      render json: { errors: "This reservation does not belong to this user" }, status: :forbidden
+    end
   end
 
   def user_reservations
